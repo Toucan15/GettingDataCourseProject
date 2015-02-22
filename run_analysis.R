8a477f234716eaf483672468277967de6fcb8903
Dan <dandroica@gmail.com>
2015-02-22 10:38:31 -0800

    added CodeBook and updated ReadMe

--- run_analysis.R
+++ run_analysis.R
@@ -1,6 +1,4 @@
-#You should create one R script called run_analysis.R that does the following. 
-
-#1.Merges the training and the test sets to create one data set.
+#1.Merge the training and the test sets to create one data set.
 ##1a. read in variable names, use only second column
 variablenames <- read.table("features.txt", colClasses = "character", nrows = 561)
 variablenames <- variablenames[,2]
@@ -20,37 +18,37 @@ extracteddata <- select(projectdata, contains("mean", ignore.case=FALSE),
 ##sort by column name (otherwise associated means and stds are far apart)
 ordered <- extracteddata[,order(names(extracteddata))]
 
-#3.Uses descriptive activity names to name the activities in the data set
-##read in subjects and activities for test data 
+#3.Use descriptive activity names to name the activities in the data set
+##3a.read in subjects and activities for test data 
 testsub <- read.table("subject_test.txt", col.names = "subject") 
 testacts <- read.table("y_test.txt", col.names = "activity")
 testcols <- cbind(testsub, testacts)
 
-##add new column for treatment (test or train)
+##3b.add new column for treatment (test or train)
 testcols$treatment <- "test"
 
-##read in subjects and activities for train data, add column for treatment
+##3c.read in subjects and activities for train data, add column for treatment
 trainsub <- read.table("subject_train.txt", col.names = "subject")
 trainacts <- read.table("y_train.txt", col.names = "activity")
 traincols <- cbind(trainsub, trainacts)
 traincols$treatment <- "train"
 
-##add all subject, activity & treatment columns together and then add to dataframe
+##3d.add all subject, activity & treatment columns together and then add to dataframe
 datacols <- rbind(testcols, traincols)
 ordered <- cbind(datacols, ordered)
 
-##convert activity factor variable to description of activity
+##3e.convert activity factor variable to description of activity
 library(car)
 ordered$activity <- Recode(ordered$activity, "1 = 'Walking'; 
         2 = 'WalkingUp'; 3 = 'WalkingDown'; 4 = 'Sitting'; 
         5 = 'Standing'; 6 = 'Laying'")
 
-#4.Appropriately labels the data set with descriptive variable names. 
+#4.Appropriately label the data set with descriptive variable names. 
 ##labelled above with feature names in step 1b
 
 #5.From the data set in step 4, create a second, independent tidy data set 
 #with the average of each variable for each activity and each subject.
-bySubjectActivity <- group_by(ordered, subject, activity)
-tidysummary <- summarise_each(bySubjectActivity, funs(mean), -treatment)
+bySubjectActivity <- group_by(ordered, treatment, subject, activity)
+tidysummary <- summarise_each(bySubjectActivity, funs(mean))
 
 write.table(tidysummary, file = "GetDataCourseProject.txt", row.names = FALSE)
